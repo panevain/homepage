0915fc86bd46d48ca4da10c40f7197e860c0fa92
Matt Newcomer <matt@panevain.dev>
2025-08-17 17:32:29 -0800

    Configure page meta and add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [{ content: "Matt Newcomer's Homepage", name: 'description' }],
+      title: 'Matt Newcomer',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', 'nuxt-quasar-ui', '@vueuse/nuxt'],
