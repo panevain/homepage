5c9f681916b0321a07de1de8ec40d26e0df2e261
Matt Newcomer <matt@panevain.dev>
2025-08-17 17:01:11 -0800

    Add VueUse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'nuxt-quasar-ui'],
+  modules: ['@nuxt/eslint', 'nuxt-quasar-ui', '@vueuse/nuxt'],
   quasar: {},
   typescript: {
     strict: true,
