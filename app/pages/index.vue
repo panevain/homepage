599d6c399946a05854c95548f5cedb94bc7b17ee
Matt Newcomer <matt@panevain.dev>
2025-08-23 16:52:44 -0800

    reduce spacing between title and text

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="q-pl-lg">
     <p class="text-h2 q-pt-md">Index</p>
-    <p class="text-body1 q-pt-md">You've reached panevain.dev</p>
+    <p class="text-body1">You've reached panevain.dev</p>
   </q-page>
 </template>
